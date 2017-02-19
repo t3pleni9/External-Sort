6eb8dd2cef85d6a69bca69f1158a959a0ebde6b0
Justin Jose <justinj@thoughtworks.com>
2017-02-20 02:06:58 +0530

    Assending global variable test

--- test/k_way_merge_sort_test.py
+++ test/k_way_merge_sort_test.py
@@ -23,6 +23,11 @@ class KWayMergeSortTest(unittest.TestCase):
     sorted_list = [i for i in self.k_way_merge.sort([2, 3, 1], [5, 6, 2], [4, 8, 0], unsorted=True, invert=True)]
     self.assertEqual(sorted_list, [8, 6, 5, 4, 3, 2, 2, 1, 0])
 
+  def test_should_use_globals_to_sort_non_sorted_lists_and_merge(self):
+    self.k_way_merge = KWayMergeSort(unsorted=True)
+    sorted_list = [i for i in self.k_way_merge.sort([2, 3, 1], [5, 6, 2], [4, 8, 0])]
+    self.assertEqual(sorted_list, [0, 1, 2, 2, 3, 4, 5, 6, 8])
+
   def test_should_use_globals_to_sort_non_sorted_lists_and_merge_in_descending(self):
     self.k_way_merge = KWayMergeSort(unsorted=True, invert=True)
     sorted_list = [i for i in self.k_way_merge.sort([2, 3, 1], [5, 6, 2], [4, 8, 0])]
